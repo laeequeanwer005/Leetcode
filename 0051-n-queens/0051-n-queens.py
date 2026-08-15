42f125b057ba5f248d97d0c4105314520879b510
laeequeanwer005 <laeequeanwer007@gmail.com>
2026-08-15 22:18:38 +0530

    Time: 11 ms (65.6%), Space: 19.7 MB (70.02%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,45 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        result = []
+
+        board = [["."] * n for _ in range(n)]
+
+        cols = set()
+        positive_diagonal = set()
+        negative_diagonal = set()
+
+        def backtrack(row):
+
+            if row == n:
+                solution = ["".join(r) for r in board]
+                result.append(solution)
+                return
+
+            for col in range(n):
+
+                if col in cols:
+                    continue
+
+                if row + col in positive_diagonal:
+                    continue
+
+                if row - col in negative_diagonal:
+                    continue
+
+                board[row][col] = "Q"
+
+                cols.add(col)
+                positive_diagonal.add(row + col)
+                negative_diagonal.add(row - col)
+
+                backtrack(row + 1)
+
+                board[row][col] = "."
+
+                cols.remove(col)
+                positive_diagonal.remove(row + col)
+                negative_diagonal.remove(row - col)
+
+        backtrack(0)
+
+        return result
